1abec2b7d45291e227f9dd245dc12aa51474449b
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-22 18:28:40 -0300

    Add staging environment file

---
+++ config/staging.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# Configure your database
+config :servus, Servus.Repo,
+  pool_size: 10
+
+config :servus, App.Endpoint,
+  url: [host: {:system, "HOSTNAME"}, port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
+
+# Do not print debug messages in production
+config :logger, level: :info
