b65c28ec368ace06ff46b150ed8614888d0cc3e3
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-22 18:28:24 -0300

    Add function init to update keyword `:url` when using module

--- lib/servus/repo.ex
+++ lib/servus/repo.ex
@@ -2,4 +2,13 @@ defmodule Servus.Repo do
   use Ecto.Repo,
     otp_app: :servus,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    Application.ensure_all_started(:ecto_sql)
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
 end
