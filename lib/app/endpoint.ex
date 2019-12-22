938869a24d9891d8766c0cf0f4aae385b35cdca1
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-22 18:37:30 -0300

    Remove channels/sockets

--- lib/app/endpoint.ex
+++ lib/app/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule App.Endpoint do
   use Phoenix.Endpoint, otp_app: :servus
 
-  socket "/socket", App.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
