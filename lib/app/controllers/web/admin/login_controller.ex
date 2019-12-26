43f187be9cb87ccdd1814beed54306c8ef385457
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-25 23:17:00 -0300

    Remove old login controller

--- lib/app/controllers/web/admin/login_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule App.Admin.LoginController do
-  use App, :controller
-
-  @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def index(conn, _params) do
-    conn
-    |> render("index.html")
-  end
-end
