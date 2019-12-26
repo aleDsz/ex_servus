e380e9cf5fcc38642ea65159603e9c7f5c0b07d1
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-25 23:22:19 -0300

    Add schema for servus permissions table

---
+++ lib/servus/schemas/servus_permissions.ex
@@ -0,0 +1,24 @@
+defmodule Servus.Schemas.ServusPermission do
+  @moduledoc """
+  Schema for Servus Permissions table
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "servus_permission" do
+    field :name, :string
+    field :description, :string
+    field :status, :string, default: "active"
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%__MODULE__{} = schema, attrs) when is_map(attrs) do
+    schema
+    |> cast(attrs, [:name, :description, :status])
+    |> validate_required([:name, :description, :status])
+  end
+end
