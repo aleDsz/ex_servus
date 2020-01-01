37191bd57730a7448debd0d1716c888016aa9835
Alexandre de Souza <alexandre@aledsz.com.br>
2020-01-01 15:58:57 -0300

    Remove controllers (live view doesn't use)

--- lib/app/controllers/admin/login_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule App.Admin.LoginController do
-  use App, :controller
-
-  @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def index(conn, _params) do
-    conn
-    |> live_render(App.Admin.LoginView, session: %{})
-  end
-end
--- lib/app/controllers/web/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule App.Web.PageController do
-  use App, :controller
-
-  @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def index(conn, _params) do
-    conn
-    |> render("index.html")
-  end
-end
