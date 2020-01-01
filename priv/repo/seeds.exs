03de40dca385ba62bacfc5a0e95fbb5d0fa39474
Alexandre de Souza <alexandre@aledsz.com.br>
2020-01-01 15:56:57 -0300

    Add default admin user seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,7 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Servus.Repo.insert!(%Servus.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Servus.Services.ServusUsers
+
+ServusUsers.create %{
+  name: "Admin",
+  email: "admin@admin.com",
+  password: "123456"
+}
