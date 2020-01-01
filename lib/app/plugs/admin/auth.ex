d896ae12f322cc6bec2126a138510f8e74d15bca
Alexandre de Souza <alexandre@aledsz.com.br>
2020-01-01 16:00:42 -0300

    Add plug module for admin panel to validate authentication using session

---
+++ lib/app/plugs/admin/auth.ex
@@ -0,0 +1,52 @@
+defmodule App.Plugs.Admin.Auth do
+  @moduledoc """
+  Validate authentication to use Admin Panel
+  """
+  require Logger
+  use Plug.Builder
+
+  import Phoenix.Controller
+
+  def init(opts), do: opts
+
+  def call(%Plug.Conn{} = conn, :admin) do
+    conn
+    |> get_flash(:user)
+    |> case do
+      nil ->
+        conn
+        |> get_session(:user)
+        |> case do
+          %{"id" => user_id} when is_binary(user_id) ->
+            conn
+
+          _not_authenticated ->
+            Logger.warn("[#{__MODULE__}] User not authenticated, redirecting...")
+
+            conn
+            |> redirect(to: "/admin/login")
+            |> halt()
+        end
+
+      %{"id" => user_id} = user when is_binary(user_id) ->
+        conn
+        |> put_session(:user, user)
+    end
+  end
+  def call(%Plug.Conn{} = conn, :login) do
+    conn
+    |> get_flash(:user)
+    |> case do
+      nil ->
+        conn
+
+      %{"id" => user_id} = user when is_binary(user_id) ->
+        Logger.debug("[#{__MODULE__}] User already authenticated, redirecting...")
+
+        conn
+        |> put_session(:user, user)
+        |> redirect(to: "/admin")
+        |> halt()
+    end
+  end
+end
