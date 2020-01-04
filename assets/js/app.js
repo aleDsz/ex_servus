6b26d550cbe90100a1ad439bac5632c4c02f7bc1
Alexandre de Souza <alexandre@aledsz.com.br>
2020-01-04 11:20:31 -0300

    Fix css and js for materialize to be compiled together

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,18 @@
+import materialize_css from "materialize-css/dist/css/materialize.min.css"
+import materialize_js from "materialize-css/dist/js/materialize.min.js"
+import css from "../css/app.css"
+
+import "jquery"
 import "phoenix_html"
 import { Socket } from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
 let liveSocket = new LiveSocket("/app", Socket, {})
 liveSocket.connect()
+
+// jQuery
+const $ = require('jquery');
+global.$ = global.jQuery = $;
 
 // application
 require("./admin/app.js")
